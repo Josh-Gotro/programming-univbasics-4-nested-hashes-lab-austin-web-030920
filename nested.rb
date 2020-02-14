31f12e5dc7eb6250ef8caa9d4a0d60a9b4f535bb
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-14 21:12:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,5 +134,5 @@ def adding_to_dennis
      }
      
      programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-    p programmer_hash[:dennis_ritchie][:languages][0]
+    p programmer_hash[:dennis_ritchie][:languages][1]
 end
